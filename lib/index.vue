fb5a49ff156c1e343a9597080813168954d261f8
yps <1106728884@qq.com>
2023-02-09 15:58:22 +0800

    废弃处理

--- lib/index.vue
+++ lib/index.vue
@@ -4,41 +4,32 @@
 */
 /*
  * @LastEditors: aFei
- * @LastEditTime: 2022-11-13 23:57:52
+ * @LastEditTime: 2023-02-09 15:51:35
 */
 <template>
-  <div class="vue-ueditor-block-plus">1</div>
+  <div class="vue-ueditor-block-plus">
+    123
+    <vue-ueditor-wrap v-model="msg" @before-init="aaaa" @ready="ready" :config="editorConfig"
+      editor-id="editor-demo-01"></vue-ueditor-wrap>
+  </div>
 </template>
-<script>
+<script setup>
+import VueUeditorWrap from 'vue-ueditor-wrap';
 import { ref, onMounted, nextTick } from "vue";
-export default {
-  name: "vueUeditorBlockPlus",
-  emits: ["change"],
-  props: {
-    // 展示按钮控件
-    showBtn: {
-      type: Boolean,
-      default: true,
-    },
-    // 抠图到图形边沿的距离
-    space: {
-      type: Number,
-      default: 10,
-    },
-    // 通过接口获取初始化图片相关信息
-    getFunction: {
-      type: Function,
-      required: true,
-    },
-    // 通过接口提交校验结果
-    setFunction: {
-      type: Function,
-      required: true,
-    },
-  },
-  setup(props, { attrs, slots, emit, expose }) {
-    return {};
-  },
+console.log(VueUeditorWrap, 'VueUeditorWrap');
+VueUeditorWrap.install()
+const emit = defineEmits([]);
+const props = defineProps({});
+const msg = ref('<h2>Hello World!</h2>');
+const editorConfig = ref({
+  UEDITOR_HOME_URL: '/UEditor/', // 访问 UEditor 静态资源的根路径，可参考常见问题1
+  serverUrl: '//ueditor.zhenghaochuan.com/cos', // 服务端接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
+});
+const aaaa = () => {
+  console.log(11111);
+}
+const ready = (ue) => {
+  console.log(ue, 'ue');
 };
 </script>
 <style lang="scss">
